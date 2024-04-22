bd04f16cf557a4a5872507a0933e3e11e2e27ee2
Shiv <shivkumarlilhare29@gmail.com>
2024-04-22 23:19:14 +0530

    Connect To The MongoDB DataBase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+// It is used for access the variable from .env
+dotenv.config()
+
+mongoose.connect(process.env.DB_CONN)
+.then(()=>{
+    console.log("MongoDB is connected");
+})
+.catch((e)=>{
+    console.log("Error is : ", e);
+})
 
 const app = express()
 
