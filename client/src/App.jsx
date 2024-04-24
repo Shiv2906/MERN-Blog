0987470766ed904203a464bfd885cadfd5b53e9b
Shiv <shivkumarlilhare29@gmail.com>
2024-04-24 12:08:05 +0530

    Create And Add Footer Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import  Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
         <Route path="/projects" element={<Projects/>}/>
     
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
