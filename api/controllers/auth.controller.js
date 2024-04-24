c5aee8a3d10da45317c32bb34190d7f460bea53d
Shiv <shivkumarlilhare29@gmail.com>
2024-04-24 17:15:29 +0530

    Create User SignIn Route And SignIn Page Functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 // define the signup function
 export const signup = async (req, res, next) => {
@@ -33,5 +34,42 @@ export const signup = async (req, res, next) => {
     res.json("Signup Successfully");
   } catch (err) {
     next(err);
+  }
+};
+
+// signIn page creation and add functinality
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All Fields Are Required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User is not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    // create token using jwt
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECURET_KEY);
+
+    // for seprate the password from user entered details we use 
+    const {password : pass, ...rest} = validUser._doc;
+
+    // store the cookie with access_token name 
+    res.status(200).cookie("access_token", token,{
+      httpOnly : true
+    }).json(rest)
+
+
+  } catch (error) {
+    next(error);
   }
 };
