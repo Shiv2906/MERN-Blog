db8ac10f61f76b3b9635e38e29a0318841c55558
Shiv <shivkumarlilhare29@gmail.com>
2024-05-03 13:59:07 +0530

    Show user post inside the dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,76 +1,75 @@
 import Post from "../model/post.model.js";
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
 
-export const create = async(req, res, next)=>{
-    if(!req.user.isAdmin)
-    {
-        return next(errorHandler(403, "You are not allowed to create a post"));
-    }
-    if(!req.body.title || !req.body.content)
-    {
-        return next(errorHandler(400, "Please provide all the required fields"));
-    }
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all the required fields"));
+  }
 
-    // create a slug for SCO purpose
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-');
+  // create a slug for SCO purpose
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
 
-    const newPost = new Post({
-        ...req.body,
-        slug,
-        userId : req.user.id,
-    });
-try {
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
     const savePost = await newPost.save();
     res.status(201).json(savePost);
-    
-} catch (error) {
-    next(error);  
-}
+  } catch (error) {
+    next(error);
+  }
 };
 
-
-export const getposts = async(req, res, next) =>{
-    try {
-        const startIndex = parseInt(req.query.startIndex) || 0;
-        const limit = parseInt(req.query.limit) || 0;
-        const sortDirection = req.query.order === 'asc' ? 1 : -1;
-        const posts = await Post.find({
-            ...(req.query.userId && {userId : req.query.userId }),
-            ...(req.query.category && {category : req.query.category }),
-            ...(req.query.slug && {slug : req.query.slug }),
-            ...(req.query.postId && {_id : req.query.postId }),
-            ...(req.query.searchTerm && {
-                $or : [
-                    {title : {$regex : req.query.searchTerm, $options : 'i'}},
-                    {content : {$regex : req.query.searchTerm, $options : 'i'}},
-                ]
-             }),
-    }).sort({updateAt : sortDirection}).skip(startIndex).limit(limit);
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 0;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updateAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
 
     const totalPosts = await Post.countDocuments();
 
     const now = new Date();
 
     const oneMonthAgo = new Date(
-        now.getFullYear(),
-        now.getMonth()-1,
-        now.getDate()
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
     );
 
     const lastMonthPosts = await Post.countDocuments({
-        createdAt : {$gte : oneMonthAgo},
+      createdAt: { $gte: oneMonthAgo },
     });
-     
+
     res.status(200).json({
-        posts,
-        totalPosts,
-        lastMonthPosts
-    })
-
-
-        
-    } catch (error) {
-        next(error);
-    }
-}
-
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,6 +5,6 @@ import { create, getposts } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create', varifyToken , create);
-router.get('/getposts' , getposts);
+router.get('/getposts', getposts)
 
 export default router;
