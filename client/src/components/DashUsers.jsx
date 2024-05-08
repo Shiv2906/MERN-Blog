b718766495d7ca82818cab941a3817132dafe4fc
Shiv <shivkumarlilhare29@gmail.com>
2024-05-09 00:44:00 +0530

    Add delete user functionality by the admin

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -44,7 +44,25 @@ export default function DashUsers() {
     }
   };
 
-  const handleDeleteUser = async(req, res) =>{
+  const handleDeleteUser = async() =>{
+    try {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+            method : "DELETE",
+        });
+         
+        const data = await res.json();
+
+        if(res.ok)
+            {
+                setUsers((prev) => prev.filter((user) => user._id != userIdToDelete))
+                setShowModel(false)
+            }else{
+                console.log(data.message);
+            }
+        
+    } catch (error) {
+        console.log(error.message);
+    }
 
   }
 
