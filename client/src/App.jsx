2400d622c632469f674da3fcbd22bebf8798d405
Shiv <shivkumarlilhare29@gmail.com>
2024-05-12 16:38:58 +0530

    Complete Search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search/>} />
 
           
         {/* privateRoute is created for Do the private dashboard when user is not sign in */}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import React from "react";
-import { useLocation, Link } from "react-router-dom";
+import React, { useEffect, useState, } from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,9 +11,24 @@ import { signoutSuccess } from "../redux/user/userSlice.js";
 export default function Header() {
   // useLocation is used for track the which page is active
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+
+useEffect(()=> {
+  const urlParams = new URLSearchParams(location.search);  const searchTermFromURL = urlParams.get('searchTerm');
+  if(searchTermFromURL)
+    {
+      setSearchTerm(searchTermFromURL);
+    }
+
+}, [location.search])
+
 
   const handleSignout = async () => {
     try {
@@ -33,6 +48,14 @@ export default function Header() {
     }
   };
 
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -44,12 +67,14 @@ export default function Header() {
         </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
 
